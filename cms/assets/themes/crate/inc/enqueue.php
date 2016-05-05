c68b2bd706a9e74bae12dc280c30132ca8262f79
Ben Byrne <ben@cornershopcreative.com>
2016-05-05 18:32:20 +0000

    Fixing bugs in enqueues, cleaning up code formatting

--- cms/assets/themes/crate/inc/enqueue.php
+++ cms/assets/themes/crate/inc/enqueue.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Enqueue stuff in frontend
  * For enqueueing scripts needed in the admin UI or login, see inc/admin.php
@@ -10,34 +9,38 @@ if (!defined('ABSPATH')) {die('Direct access not allowed');}
 add_action('wp_enqueue_scripts', 'crate_enqueue_assets');
 function crate_enqueue_assets() {
 
-	// Use CDN jQuery in the hopes the client already has it or at least it gets to them fast
-	wp_deregister_script('jquery');
-	wp_register_script('jquery', "//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js", '2.2.3', true);
-
 	//our scripts and styles
-	wp_register_script('plugins', get_template_directory_uri() . '/js/plugins.min.js', array('jquery'), false, true);
-	wp_register_script('modernizr', get_template_directory_uri() . '/js/modernizr.js', false, '3.3.1', false);
-	wp_register_script('main', get_template_directory_uri() . '/js/main.min.js', array('jquery', 'plugins'), false, true);
-	wp_register_style('crate', get_template_directory_uri() . '/css/core.min.css', array(), '3.1.1', 'all'); //could also be "screen"
-	wp_register_style('print', get_template_directory_uri() . '/css/print.min.css', array(), '3.1.1', 'print');
+	wp_register_script('plugins',   get_template_directory_uri() . '/js/plugins.min.js', array('jquery'), false, true);
+	wp_register_script('modernizr', get_template_directory_uri() . '/js/modernizr.js',   false, '3.3.1', false);
+	wp_register_script('main',      get_template_directory_uri() . '/js/main.min.js',    array('jquery', 'plugins'), false, true);
+	wp_register_style( 'crate',     get_template_directory_uri() . '/css/core.min.css',  array(), '3.1.1', 'all');	//could also be "screen"
+	wp_register_style( 'print',     get_template_directory_uri() . '/css/print.min.css', array(), '3.1.1', 'print');
 
 	//during development, avoid minified versions and run livereoad
-	if (defined('SCRIPT_DEBUG') && SCRIPT_DEBUG) {
+	//plugins.min.js is still loaded as minified, since that's all third-party code that shouldn't need debugging
+	if ( defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ) {
 
-		wp_deregister_script('main');
-		wp_register_script('main', get_template_directory_uri() . '/js/main.js', array('jquery', 'plugins'), false, true);
+		wp_deregister_script( 'main' );
+		wp_deregister_style( 'crate' );
+		wp_deregister_style( 'print' );
 
-		wp_deregister_style('crate');
-		wp_deregister_style('print');
-		wp_register_style('crate', get_template_directory_uri() . '/css/core.css', array(), '3.1.1', 'all'); //could also be "screen"
-		wp_register_style('print', get_template_directory_uri() . '/css/print.css', array(), '3.1.1', 'print');
+		wp_register_script('main',    get_template_directory_uri() . '/js/main.js',    array('jquery', 'plugins'), false, true);
+		wp_register_style( 'crate',   get_template_directory_uri() . '/css/core.css',      array(), '3.1.1', 'all');	//could also be "screen"
+		wp_register_style( 'print',   get_template_directory_uri() . '/css/print.css',     array(), '3.1.1', 'print');
 
-		if (defined('LIVERELOAD_PORT')) {
+		if ( defined('LIVERELOAD_PORT') ) {
 			wp_enqueue_script('livereload', '//' . $_SERVER['SERVER_NAME'] . ':' . LIVERELOAD_PORT . "/livereload.js", array('main'), false, true);
 		}
 	}
 
-	if (!is_admin()) {
+	if ( !is_admin() ) {
+
+		//use CDN jQuery in the hopes the client already has it, or at least it gets there fast
+		//note that going past 1.8.x in jquery can sometimes lead to problems
+		//as can loading 3rd party jQuery that still clings to $, which is why we only do this on the front end
+		wp_deregister_script('jquery');
+		wp_register_script('jquery', "//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js", '2.2.3', true);
+
 		wp_enqueue_script('modernizr');
 		wp_enqueue_script('main');
 		wp_enqueue_style('crate');
@@ -47,19 +50,3 @@ function crate_enqueue_assets() {
 
 	}
 }
-
-/**
- * Yepnope is no longer included in Modernizr, so this won't work anymore.
- * Leaving here for posterity
- */
-function crate_selectivizr() {
-	if (wp_script_is('modernizr', 'done')):
-	?><script>
-	yepnope({
-		test: Modernizr.generatedcontent && Modernizr.lastchild,
-		nope: "<?php echo get_template_directory_uri() . '/js/selectivizr.min.js' ?>"
-	});
-</script><?php
-endif;
-}
-//add_action('wp_head', 'crate_selectivizr', 9);
