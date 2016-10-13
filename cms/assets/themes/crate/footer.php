38a3dbd03d00911f9a35b9e4b6a541c79a919b49
Kenji Garland <kenji@cornershopcreative.com>
2016-10-13 18:13:34 +0000

    Add and style footer (first pass, without right-hand logos/links)

--- cms/assets/themes/crate/footer.php
+++ cms/assets/themes/crate/footer.php
@@ -14,7 +14,40 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
+		<div class="container">
+			<nav class="nav-footer">
+				<?php
+
+				wp_nav_menu( array(
+					'theme_location' => 'footer-1',
+					'menu_id' => 'footer-menu-1',
+					'menu_class' => 'menu menu-footer'
+				) );
+
+				wp_nav_menu( array(
+					'theme_location' => 'footer-1',
+					'menu_id' => 'footer-menu-2',
+					'menu_class' => 'menu menu-footer'
+				) );
+
+				wp_nav_menu( array(
+					'theme_location' => 'footer-1',
+					'menu_id' => 'footer-menu-3',
+					'menu_class' => 'menu menu-footer'
+				) );
+
+				?>
+			</nav>
+			<div class="site-info">
+				<div class="contact">
+					<p>&copy; Generation to Generation 2016. All rights reserved.<br/>
+						P.O. Box 29542 | San Fransisco, CA 94129</p>
+				</div>
+				<div class="credits">
+					<p>Designed by Ronik<br/>
+						Crafted by Cornershop Creative</p>
+				</div>
+			</div>
 		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
