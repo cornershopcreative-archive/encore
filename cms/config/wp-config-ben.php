e59a3d321cad42afed38861f2e860cf813a04aa4
Ben Byrne <ben@cornershopcreative.com>
2016-10-26 22:15:49 +0000

    Configure WordPress (setupsite)

---
+++ cms/config/wp-config-ben.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The base configuration of WordPress for Cornershop Dev
+ */
+
+/*
+ * CHANGE THESE
+ */
+define('DB_NAME', 'ben_encore');
+define('DB_USER', 'ben');
+define('DB_PASSWORD', 'ivycrown');
+define('DB_HOST',     'localhost');
+
+/**
+ * For developers: WordPress debugging modes, caching, etc.
+ */
+define('WP_DEBUG',         true);
+define('WP_DEBUG_LOG',     true);
+define('WP_DEBUG_DISPLAY', false);
+define('WP_CACHE',         false);
+define('SAVEQUERIES',      true);
+define('SCRIPT_DEBUG',     true);
