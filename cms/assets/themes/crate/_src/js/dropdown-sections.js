5f0df0a1bf26c87e9908c5f75b80ede27c357965
Kenji Garland <kenjigarland@gmail.com>
2017-02-08 15:07:54 +0000

    Make dropdown section menus open on hover, not just on click/tap

--- cms/assets/themes/crate/_src/js/dropdown-sections.js
+++ cms/assets/themes/crate/_src/js/dropdown-sections.js
@@ -5,15 +5,25 @@ module.exports = function( $ ) {
 		var $menu = $( this ).find( '.section-dropdown-menu' );
 		var $options = $menu.find( '.option' );
 		var $sets = $( this ).find( '.section-dropdown-set' );
+		var close_timer;
 
 		$( document ).click( function( e ) {
-			$menu.removeClass( 'is-open' );
+			$menu.removeClass( 'is-open is-clicked' );
 		} );
 
 		$menu.click( function( e ) {
 			e.preventDefault();
 			e.stopPropagation();
-			$menu.toggleClass( 'is-open' );
+			$menu.addClass( 'is-open is-clicked' );
+		} ).hover( function() {
+			clearTimeout( close_timer );
+			$menu.addClass( 'is-open' );
+		}, function() {
+			close_timer = setTimeout( function() {
+				if ( ! $menu.is( '.is-clicked' ) ) {
+					$menu.removeClass( 'is-open' );
+				}
+			}, 300 );
 		} );
 
 		$options.click( function( e ) {
@@ -25,7 +35,7 @@ module.exports = function( $ ) {
 			// Change the text of the current option to match this dropdown option's
 			// text.
 			$menu.find( '.current-option' ).html( $( this ).html() );
-			$menu.removeClass( 'is-open' );
+			$menu.removeClass( 'is-open is-clicked' );
 		} );
 
 		$options.eq(0).click();
--- cms/assets/themes/crate/inc/enqueue.php
+++ cms/assets/themes/crate/inc/enqueue.php
@@ -14,7 +14,7 @@ function crate_enqueue() {
 
   if ( ! is_admin() ) {
 		wp_enqueue_style( 'crate_style', get_template_directory_uri() . '/css/crate.css', array(), '4.4.6' );
-		wp_enqueue_script( 'crate', get_template_directory_uri() . '/js/crate.js', array('jquery'), '4.4.6', true );
+		wp_enqueue_script( 'crate', get_template_directory_uri() . '/js/crate.js', array('jquery'), '4.4.7', true );
 
 		// put the AJAX endpoint URL into theme.ajaxurl
 		wp_localize_script( 'crate', 'crate', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
