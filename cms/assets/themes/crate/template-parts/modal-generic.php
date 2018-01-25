786dc5055958396aa46f2d93ddd611a3c48d6272
Kenji Garland <kenji@cornershopcreative.com>
2018-01-25 20:32:52 +0000

    Change hard-coded GF form IDs to match IDs on prod

--- cms/assets/themes/crate/template-parts/modal-generic.php
+++ cms/assets/themes/crate/template-parts/modal-generic.php
@@ -3,19 +3,19 @@
 
 	<aside id="signup-modal-navigation" class="modal cta-huge">
 		<div>
-			<?php gravity_form( 4, false, false, false, false, true ); ?>
+			<?php gravity_form( 8, false, false, false, false, true ); ?>
 		</div>
 	</aside>
 
 	<aside id="signup-modal-hero" class="modal cta-huge">
 		<div>
-			<?php gravity_form( 5, false, false, false, false, true ); ?>
+			<?php gravity_form( 9, false, false, false, false, true ); ?>
 		</div>
 	</aside>
 
 	<aside id="signup-modal-automatic" class="modal cta-huge">
 		<div>
-			<?php gravity_form( 7, false, false, false, false, true ); ?>
+			<?php gravity_form( 11, false, false, false, false, true ); ?>
 		</div>
 	</aside>
 
